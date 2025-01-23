c71b82dbac793bdf8a2f00c95b74a4dc44bb59d1
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-01-23 17:00:09 +0100

    articles fatti

--- src/components/ArticleDetails.tsx
+++ src/components/ArticleDetails.tsx
@@ -4,24 +4,24 @@ import { ArticlesT } from '../types/ArticlesT'
 
 interface ArticleDetailsProps {
     article: ArticlesT
+    
 
 }
 
 const ArticleDetails = (props : ArticleDetailsProps)  => {
     return (
         <Col xs={12} md={4}>
-             <Card >
-            <Card.Img variant="top" src={props.article.results.image_url} alt={props.article.results.title} />
+        <Card>
+            <Card.Img variant="top" src={props.article.image_url} alt={props.article.title} />
             <Card.Body>
-                <Card.Title>{props.article.results.title}</Card.Title>
+                <Card.Title>{props.article.title}</Card.Title>
                 <Card.Text>
                     {props.article.summary}
                 </Card.Text>
                 <Card.Link href={props.article.url} target="_blank">Read more</Card.Link>
             </Card.Body>
         </Card>
-        </Col>
-        
+    </Col>
     )
 }
 
